ae31af0f6374ffb1c77b9fa7bcfb04b90f744712
huangshiheng <huangshiheng@lixiang.com>
2023-09-02 21:54:35 +0800

    fix(mode): enter long mode

---
+++ boot/boot_long.asm
@@ -0,0 +1,120 @@
+%define FREE_SPACE 0x9000
+[bits 16]
+%define PAGE_PRESENT    (1 << 0)
+%define PAGE_WRITE      (1 << 1)
+
+IDT:
+    .Length dw 0
+    .Base   dd 0
+
+switch_long_mode:
+    xor ax, ax
+    ; Set up segment registers.
+    mov ss, ax
+    ; Set up stack so that it starts below Main.
+    mov sp, switch_long_mode
+    mov ds, ax
+    mov es, ax
+    mov fs, ax
+    mov gs, ax
+    cld
+    mov edi, FREE_SPACE ; Point edi to a free space bracket.
+    call switch_long_mode_start
+
+switch_long_mode_start:
+    push di                           ; REP STOSD alters DI.
+    mov ecx, 0x1000
+    xor eax, eax
+    cld
+    rep stosd
+    pop di                            ; Get DI back.
+
+    ; Build the Page Map Level 4.
+    ; es:di points to the Page Map Level 4 table.
+    lea eax, [es:di + 0x1000]         ; Put the address of the Page Directory Pointer Table in to EAX.
+    or eax, PAGE_PRESENT | PAGE_WRITE ; Or EAX with the flags - present flag, writable flag.
+    mov [es:di], eax                  ; Store the value of EAX as the first PML4E.
+
+    ; Build the Page Directory Pointer Table.
+    lea eax, [es:di + 0x2000]         ; Put the address of the Page Directory in to EAX.
+    or eax, PAGE_PRESENT | PAGE_WRITE ; Or EAX with the flags - present flag, writable flag.
+    mov [es:di + 0x1000], eax         ; Store the value of EAX as the first PDPTE.
+
+    ; Build the Page Directory.
+    lea eax, [es:di + 0x3000]         ; Put the address of the Page Table in to EAX.
+    or eax, PAGE_PRESENT | PAGE_WRITE ; Or EAX with the flags - present flag, writeable flag.
+    mov [es:di + 0x2000], eax         ; Store to value of EAX as the first PDE.
+
+    push di                           ; Save DI for the time being.
+    lea di, [di + 0x3000]             ; Point DI to the page table.
+    mov eax, PAGE_PRESENT | PAGE_WRITE    ; Move the flags into EAX - and point it to 0x0000.
+
+    ; Build the Page Table.
+LoopPageTable:
+    mov [es:di], eax
+    add eax, 0x1000
+    add di, 8
+    cmp eax, 0x200000                 ; If we did all 2MiB, end.
+    jb LoopPageTable
+
+    pop di                            ; Restore DI.
+    ; Disable IRQs
+    mov al, 0xFF                      ; Out 0xFF to 0xA1 and 0x21 to disable all IRQs.
+    out 0xA1, al
+    out 0x21, al
+    nop
+    nop
+    lgdt [GDT_descriptor]                  ; Load GDT.Pointer defined below.
+    lidt [IDT]                        ; Load a zero length IDT so that any NMI causes a triple fault.
+
+    ; Enter long mode.
+    mov eax, 10100000b                ; Set the PAE and PGE bit.
+    mov cr4, eax
+
+    mov edx, edi                      ; Point CR3 at the PML4.
+    mov cr3, edx
+
+    mov ecx, 0xC0000080               ; Read from the EFER MSR. 
+    rdmsr    
+
+    or eax, 0x00000100                ; Set the LME bit.
+    wrmsr
+
+    mov ebx, cr0                      ; Activate long mode -
+    or ebx,0x80000001                 ; - by enabling paging and protection simultaneously.
+    mov cr0, ebx                    
+
+    jmp LONG_CODE_SEG:LongMode
+
+
+[BITS 64]
+[extern kernel_main]
+LongMode:
+    mov ax, LONG_DATA_SEG
+    mov ds, ax
+    mov es, ax
+    mov fs, ax
+    mov gs, ax
+    mov ss, ax
+
+    ; Blank out the screen to a blue color.
+    mov edi, 0xB8000
+    mov rcx, 500                      ; Since we are clearing uint64_t over here, we put the count as Count/4.
+    mov rax, 0x1F201F201F201F20       ; Set the value to set the screen to: Blue background, white foreground, blank spaces.
+    rep stosq                         ; Clear the entire screen. 
+ 
+    ; ; Display "Hello World!"
+    mov edi, 0x00b8000              
+ 
+    mov rax, 0x1F6C1F6C1F651F48    
+    mov [edi],rax
+ 
+    mov rax, 0x1F6F1F571F201F6F
+    mov [edi + 8], rax
+
+    mov rax, 0x1F211F641F6C1F72
+    mov [edi + 16], rax
+
+    call kernel_main
+
+    jmp $
