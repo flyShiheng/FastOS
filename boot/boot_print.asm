25766be16d3921d41f25801da01b3f2294f00761
huangshiheng <huangshiheng@lixiang.com>
2023-09-02 17:09:17 +0800

    fix(print): boot_print.asm

---
+++ boot/boot_print.asm
@@ -0,0 +1,51 @@
+[bits 16]
+print_real_string:
+    pusha
+print_start:
+    mov al, [bx]  ; bx中保存着数据的首地址
+    cmp al, 0     ; 为空就返回
+    je print_done
+    mov ah, 0x0e  ; 显示char字符
+    int 0x10
+    add bx, 1
+    jmp print_start
+print_done:
+    popa
+    ret
+
+print_real_mode_nl:
+    pusha
+    mov ah, 0x0e
+    mov al, 0x0a  ; ASCII 0x0a = 10 = LF 换行
+    int 0x10
+    popa
+    ret
+
+set_print_init:
+    pusha
+    mov ah, 0x00
+    mov al, 0x03
+    int 0x10
+    popa
+    ret
+
+set_print_real_mode_page:
+    pusha
+    mov ah, 0x02
+    mov bh, 0x0
+    int 0x10
+    popa
+    ret
+
+set_print_real_mode_cursor:
+    pusha
+    mov ah, 0x03
+    mov bh, 0x0
+    int 0x10
+
+    mov ah, 0x02
+    ; mov dh, 0x0  ; 行
+    mov dl, 0  ; 列
+    int 0x10
+    popa
+    ret
--- boot/boot_sect.asm
+++ boot/boot_sect.asm
@@ -14,57 +14,6 @@ start:
     call print_real_string
     jmp $
 
-print_real_string:
-    pusha
-print_start:
-    mov al, [bx]  ; bx中保存着数据的首地址
-    cmp al, 0     ; 为空就返回
-    je print_done
-    mov ah, 0x0e  ; 显示char字符
-    int 0x10
-    add bx, 1
-    jmp print_start
-print_done:
-    popa
-    ret
-
-print_real_mode_nl:
-    pusha
-    mov ah, 0x0e
-    mov al, 0x0a  ; ASCII 0x0a = 10 = LF 换行
-    int 0x10
-    popa
-    ret
-
-set_print_init:
-    pusha
-    mov ah, 0x00
-    mov al, 0x03
-    int 0x10
-    popa
-    ret
-
-set_print_real_mode_page:
-    pusha
-    mov ah, 0x02
-    mov bh, 0x0
-    int 0x10
-    popa
-    ret
-
-set_print_real_mode_cursor:
-    pusha
-    mov ah, 0x03
-    mov bh, 0x0
-    int 0x10
-
-    mov ah, 0x02
-    ; mov dh, 0x0  ; 行
-    mov dl, 0  ; 列
-    int 0x10
-    popa
-    ret
-
 [bits 16]
 load_kernel:
     pusha
@@ -74,6 +23,7 @@ load_kernel:
     popa
     ret
 
+%include "boot/boot_print.asm"
 %include "boot/boot_disk.asm"
 
 MSG_REAL_MODE db "Started in 16-bit Real Mode", 0
