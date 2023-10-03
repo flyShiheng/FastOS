d0e7b041a66a05f26e709a66b6ce24ad1ffdd007
huangshiheng <huangshiheng@lixiang.com>
2023-10-03 11:10:36 +0800

    load cr3

--- boot/boot_kernel.asm
+++ boot/boot_kernel.asm
@@ -2,10 +2,35 @@
 boot_start:
     call print_real_mode_nl
     call set_print_real_mode_cursor
+
+    mov ebx, 0       ; ebx=0，ebx
+    mov ax, 0x00
+    mov es, ax
+    mov di, 0x8000
+
+Label_Get_Mem_Struct:
+    mov eax, 0x0E820         ; 功能号 ax=E820h 从硬件查询系统地址映射信息
+    mov ecx, 24              ; struct 的大小
+    mov edx, 0x534D4150      ; 这是一个魔法数字，即字符串“SMAP”
+    ; mov [es:di + 20], dword 1
+    int 15h
+    jc  Label_Get_Mem_Fail   ; 读取成功时 CF=0，继续向下执行，否则跳转到错误分支
+    add di, 24               ; 读取下一个 struct
+
+    cmp ebx,  0
+    jne Label_Get_Mem_Struct  ; ebx!=0，表示还未遍历完，继续遍历，否则继续向下执行
+    jmp Label_Get_Mem_OK      ; 已遍历完，跳转到成功分支
+
+Label_Get_Mem_OK:
     mov bx, MSG_BOOT_START
     call print_real_string
 
     call switch_long_mode
+    jmp $
+
+Label_Get_Mem_Fail:
+    mov bx, MSG_MEM_ERROR
+    call print_real_string
     jmp $
 
 %include "boot/boot_print.asm"
@@ -13,3 +38,4 @@ boot_start:
 %include "boot/boot_long.asm"
 
 MSG_BOOT_START db "Boot Start", 0
+MSG_MEM_ERROR db "Boot GET MEM ERROR", 0
--- mem/page.c
+++ mem/page.c
@@ -15,34 +15,49 @@ void paging_indexer_assign(paging_indexer_t* indexer, void* address) {
 
 // range 32 * 512 * 512 * 4k = 2^5 * 2^9 * 2^9 * 2^12 = 2^35 = 32G
 void mem_init2() {
-    uint64_t* pml4 = 0x8000000;  // 1   -- 4k
-    uint64_t* pml3 = 0x8001000;  // 32  -- 4k
-    uint64_t* pml2 = 0x8002000;  // 512 -- 32 * 4k = 128k = 0x20000
-    uint64_t* pml1 = 0x8003000;  // 512 -- 32 * 512 * 4k = 64M = 0x4000000
+    uint64_t* pml4 = (uint64_t*)0x7e00000;
+    uint64_t* pml3 = (uint64_t*)0x7e01000;
+    uint64_t* pml2 = (uint64_t*)0x7e02000;
+    uint64_t* pml1 = (uint64_t*)0x7e03000;
     uint64_t pml4_phys = get_physaddr((uint64_t)pml4);
     uint64_t pml3_phys = get_physaddr((uint64_t)pml3);
     uint64_t pml2_phys = get_physaddr((uint64_t)pml2);
     uint64_t pml1_phys = get_physaddr((uint64_t)pml1);
 
-    pml4[0] = pml3_phys | PTE_P | PTE_W;
-    pml3[0] = pml2_phys | PTE_P | PTE_W;
-    pml2[0] = pml1_phys | PTE_P | PTE_W;
-    // for (uint64_t i = 0; i < 32; i++) {
-    //     pml3[i] = (pml2_phys + i * 0x1000) | PTE_P | PTE_W;
-    // }
-    // for (uint64_t i = 0; i < 512; i++) {
-    //     pml2[i] = (pml1_phys + i * 0x1000) | PTE_P | PTE_W;
-    // }
+    pml4[0] = (uint64_t)pml3_phys | PTE_P | PTE_W;
+    pml3[0] = (uint64_t)pml2_phys | PTE_P | PTE_W;
+    pml2[0] = (uint64_t)pml1_phys | PTE_P | PTE_W;
+
     for (uint64_t i = 0; i < 512; i++) {
-        pml1[i] = (0 + i * 0x1000) | PTE_P | PTE_W;
+        uint64_t addr = (0 + i * 0x1000) | PTE_P | PTE_W;
+        pml1[i] = addr;
     }
+    pml1 = 0x7e03000 + 0x1000 * 63;
+    pml1_phys = get_physaddr((uint64_t)pml1);
+    pml2[63] = pml1_phys | PTE_P | PTE_W;
+    for (uint64_t i = 0; i < 512; i++) {
+        pml1[i] = (0x7000000 + i * 0x1000) | PTE_P | PTE_W;
+    }
+
     printk("\nmem_init2 phys pml4:%x, pml3:%x, pml2:%x, pml1:%x\n",
         pml4_phys, pml3_phys, pml2_phys, pml1_phys);
-    __asm__ volatile("mov %0, %%cr3" : : "a"(pml4));
-    __asm__ volatile ("cli; hlt");
+    __asm__ volatile("mov %0, %%cr3" : : "a"(pml4_phys));
 }
 
 void mem_init() {
+    MemoryMapEntry* mmap = (MemoryMapEntry*)0x8000;
+
+    // 遍历内存映射表
+    while ((mmap->type <= 4) && (mmap->type >= 1) && (mmap->length != 0)) {
+        // printk("MemoryMapEntry address: %x\n", mmap);
+        if (mmap->type == 1) {
+            printk("Base Address: %x, Length: %x\n", mmap->address, mmap->length);
+        }
+        mmap++;
+    }
+
+    get_physaddr(0x7e00000);
+
     uint64_t* pml4 = 0x101000;  // 1M + 4k
     uint64_t* pml3 = 0x102000;
     uint64_t* pml2 = 0x103000;
@@ -53,26 +68,26 @@ void mem_init() {
     for (uint64_t i = 0; i < 512; i++) {
         pml1[i] = (0 + i * 0x1000) | PTE_P | PTE_W;
     }
-    pml1 = 0x105000;
-    pml2[64] = (uint64_t)pml1 | PTE_P | PTE_W;
+    pml1 = 0x106000;
+    pml2[63] = (uint64_t)pml1 | PTE_P | PTE_W;
     for (uint64_t i = 0; i < 512; i++) {
-        pml1[i] = (0x8000000 + i * 0x1000) | PTE_P | PTE_W;  // 128M = 0x8000000
+        pml1[i] = (0x7000000 + i * 0x1000) | PTE_P | PTE_W;  // 128M = 0x8000000
     }
     __asm__ volatile("mov %0, %%cr3" : : "a"(pml4));
-    // mem_init2();
+    mem_init2();
 }
 
 uint64_t get_physaddr(void *virt) {
     paging_indexer_t page_index;
     paging_indexer_assign(&page_index, virt);
-    // printk("\get_physaddr page_index pml4:%d pml3:%d pml2:%d pml1:%d\n", page_index.pml4, page_index.pml3, page_index.pml2, page_index.pml1);
+    // printk("get_physaddr page_index pml4:%d pml3:%d pml2:%d pml1:%d\n", page_index.pml4, page_index.pml3, page_index.pml2, page_index.pml1);
 
     uint64_t* pml4 = (uint64_t*)get_phys_mem_load();
     uint64_t* pml3 = (uint64_t*)(pml4[page_index.pml4] & ~0xFFF);
     uint64_t* pml2 = (uint64_t*)(pml3[page_index.pml3] & ~0xFFF);
     uint64_t* pml1 = (uint64_t*)(pml2[page_index.pml2] & ~0xFFF);
     uint64_t  page = pml1[page_index.pml1];
-    // printk("\get_physaddr addr pml4:%x pml3:%x pml2:%x pml1:%x page:%x\n", pml4, pml3, pml2, pml1, page);
+    //printk("get_physaddr addr pml4:%x pml3:%x pml2:%x pml1:%x page:%x\n", pml4, pml3, pml2, pml1, page);
     return ((page & ~0xFFF) | ((uint64_t)virt & 0xFFF));
 }
 
